d68aaff7d4a3a9f5d3a7d9512ede78b591ce26e3
Tyler Christiansen <code@tylerc.me>
2016-12-18 23:43:18 -0500

    Convert tabs to spaces.

--- SSHClient.py
+++ SSHClient.py
@@ -21,99 +21,99 @@ import os
 TIME_OUT = 10
 
 class Client(object):
-	def __init__(self, session):
-		self._session = session
+    def __init__(self, session):
+        self._session = session
 
 
 
 class SSHClient(Client):
-	def __init__(self, session):
-		super(SSHClient, self).__init__(session)
-		self._socket = None
-		logging.debug("Client: Client Created")
-		
-	def connect(self, ip, port, size):
-		self._size = size
-		self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self._socket.settimeout(TIME_OUT)
-		self._socket.connect((ip, port))
-		logging.debug("SSHClient: Connected to {0}:{1}".format(ip,port))
-		
-		
-	def get_transport(self):
-		transport = paramiko.Transport(self._socket)
-		transport.set_keepalive(10)
-		transport.start_client()
-		return transport
+    def __init__(self, session):
+        super(SSHClient, self).__init__(session)
+        self._socket = None
+        logging.debug("Client: Client Created")
+        
+    def connect(self, ip, port, size):
+        self._size = size
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.settimeout(TIME_OUT)
+        self._socket.connect((ip, port))
+        logging.debug("SSHClient: Connected to {0}:{1}".format(ip,port))
+        
+        
+    def get_transport(self):
+        transport = paramiko.Transport(self._socket)
+        transport.set_keepalive(10)
+        transport.start_client()
+        return transport
 
-	def start_session(self, user, auth_secret):
-		logging.debug("SSHClient: Authenticating session.")
-		try:
-			transport = self.get_transport()
-			if isinstance(auth_secret, basestring):
-				transport.auth_password(user, auth_secret)
-			else:
-				try:
-					transport.auth_publickey(user, auth_secret)
-				# Failed to authenticate with SSH key, so
-				# try a password instead.
-				except paramiko.ssh_exception.AuthenticationException:
-					transport.auth_password(user, getpass.getpass())
-			self._start_session(transport)
-		except EOFError as exc:
-			logging.error('Received EOFError.  Assuming bad SSH implementation.')
-			logging.error('Original Erorr: %s', exc)
-			self._handle_exception(transport)
-		except Exception as e:
-			logging.error(e)
-			self._handle_exception(transport)
-			raise e
+    def start_session(self, user, auth_secret):
+        logging.debug("SSHClient: Authenticating session.")
+        try:
+            transport = self.get_transport()
+            if isinstance(auth_secret, basestring):
+                transport.auth_password(user, auth_secret)
+            else:
+                try:
+                    transport.auth_publickey(user, auth_secret)
+                # Failed to authenticate with SSH key, so
+                # try a password instead.
+                except paramiko.ssh_exception.AuthenticationException:
+                    transport.auth_password(user, getpass.getpass())
+            self._start_session(transport)
+        except EOFError as exc:
+            logging.error('Received EOFError.  Assuming bad SSH implementation.')
+            logging.error('Original Erorr: %s', exc)
+            self._handle_exception(transport)
+        except Exception as e:
+            logging.error(e)
+            self._handle_exception(transport)
+            raise e
 
-	def _handle_exception(self, transport):
-		self._session.close_session()
-		if transport:
-			transport.close()
-		self._socket.close()
+    def _handle_exception(self, transport):
+        self._session.close_session()
+        if transport:
+            transport.close()
+        self._socket.close()
 
-			
-	def _start_session(self, transport):
-		chan = transport.open_session()
-		cols, rows = self._size
-		chan.get_pty('xterm', cols, rows)
-		chan.invoke_shell()
-		self.interactive_shell(chan)
-		chan.close()
-		self._session.close_session()
-		transport.close()
-		self._socket.close()
+            
+    def _start_session(self, transport):
+        chan = transport.open_session()
+        cols, rows = self._size
+        chan.get_pty('xterm', cols, rows)
+        chan.invoke_shell()
+        self.interactive_shell(chan)
+        chan.close()
+        self._session.close_session()
+        transport.close()
+        self._socket.close()
 
 
-	def interactive_shell(self, chan):
-		# Handle session IO
-		sys.stdout.flush()
-		try:
-			signal.signal(signal.SIGHUP, self._session.kill_session)
-			oldtty = termios.tcgetattr(sys.stdin)
-			tty.setraw(sys.stdin.fileno())
-			tty.setcbreak(sys.stdin.fileno())
-			chan.settimeout(0.0)
-			while True:
-				r, w, e = select.select([chan, sys.stdin], [], [])
-				if chan in r:
-					try:
-						x = chan.recv(1024)
-						if len(x) == 0:
-							break
-						sys.stdout.write(x)
-						sys.stdout.flush()
-					except socket.timeout:
-						break
-				if sys.stdin in r:
-					x = os.read(sys.stdin.fileno(), 1)
-					if len(x) == 0:
-						break
-					chan.send(x)
-			termios.tcsetattr(sys.stdin, termios.TCSADRAIN, oldtty)
-		except Exception as e:
-			logging.error(e)
-			raise e
+    def interactive_shell(self, chan):
+        # Handle session IO
+        sys.stdout.flush()
+        try:
+            signal.signal(signal.SIGHUP, self._session.kill_session)
+            oldtty = termios.tcgetattr(sys.stdin)
+            tty.setraw(sys.stdin.fileno())
+            tty.setcbreak(sys.stdin.fileno())
+            chan.settimeout(0.0)
+            while True:
+                r, w, e = select.select([chan, sys.stdin], [], [])
+                if chan in r:
+                    try:
+                        x = chan.recv(1024)
+                        if len(x) == 0:
+                            break
+                        sys.stdout.write(x)
+                        sys.stdout.flush()
+                    except socket.timeout:
+                        break
+                if sys.stdin in r:
+                    x = os.read(sys.stdin.fileno(), 1)
+                    if len(x) == 0:
+                        break
+                    chan.send(x)
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, oldtty)
+        except Exception as e:
+            logging.error(e)
+            raise e
--- session.py
+++ session.py
@@ -14,55 +14,55 @@ import os
 from SSHClient import SSHClient
 
 class Session(object):
-	""" Base class for sessions
-		Different type of sessions to be
-		added later
-	"""
-	
-	def __init__(self,aker_core,host,uuid):
-		self.aker= aker_core
-		self.host = host
-		self.host_user = self.aker.user.ssh_hosts[host]['username']
-		self.host_port = int(self.aker.user.ssh_hosts[host]['port'])
-		self.uuid = uuid
-		logging.debug("Session: Base Session created")
-		#TODO : Add UUID shit 
-		
-	def connect(self, size):
-		self._client.connect(self.host, self.host_port, size)
+    """ Base class for sessions
+        Different type of sessions to be
+        added later
+    """
+    
+    def __init__(self,aker_core,host,uuid):
+        self.aker= aker_core
+        self.host = host
+        self.host_user = self.aker.user.ssh_hosts[host]['username']
+        self.host_port = int(self.aker.user.ssh_hosts[host]['port'])
+        self.uuid = uuid
+        logging.debug("Session: Base Session created")
+        #TODO : Add UUID shit 
         
-	def start_session(self):
-		raise NotImplementedError
-		
-	def close_session(self):
-		self.aker.session_end_callback(self)
-		
-	def kill_session(self, signum, stack):
-		#TODO : Change behavoir to show screen again
-		logging.debug("Session: Session Killed")
-		self.close_session()
-		os.kill(os.getpid(), signal.SIGKILL)
+    def connect(self, size):
+        self._client.connect(self.host, self.host_port, size)
+        
+    def start_session(self):
+        raise NotImplementedError
+        
+    def close_session(self):
+        self.aker.session_end_callback(self)
+        
+    def kill_session(self, signum, stack):
+        #TODO : Change behavoir to show screen again
+        logging.debug("Session: Session Killed")
+        self.close_session()
+        os.kill(os.getpid(), signal.SIGKILL)
 
 
 class SSHSession(Session):
-	""" Wrapper around SSHClient instantiating 
-		a new SSHClient instance everytime
-	"""
-	
-	def __init__(self, aker_core, host,uuid):
-		super(SSHSession, self).__init__(aker_core, host,uuid)
-		self._client = SSHClient(self)
-		logging.debug("Session: SSHSession created")
+    """ Wrapper around SSHClient instantiating 
+        a new SSHClient instance everytime
+    """
+    
+    def __init__(self, aker_core, host,uuid):
+        super(SSHSession, self).__init__(aker_core, host,uuid)
+        self._client = SSHClient(self)
+        logging.debug("Session: SSHSession created")
 
-		
-	def start_session(self):
-		try:
-			auth_secret = self.aker.user.get_priv_key()
-		# currently, if no SSH public key exists, an ``Exception``
-		# is raised.  Catch it and try a password.
-		except Exception as exc:
-			if str(exc) == 'Core: Invalid Private Key':
-				auth_secret = getpass.getpass("Password: ")
-			else:
-				raise
-		self._client.start_session(self.host_user, auth_secret)
+        
+    def start_session(self):
+        try:
+            auth_secret = self.aker.user.get_priv_key()
+        # currently, if no SSH public key exists, an ``Exception``
+        # is raised.  Catch it and try a password.
+        except Exception as exc:
+            if str(exc) == 'Core: Invalid Private Key':
+                auth_secret = getpass.getpass("Password: ")
+            else:
+                raise
+        self._client.start_session(self.host_user, auth_secret)
